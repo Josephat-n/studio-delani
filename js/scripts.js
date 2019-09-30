0ee9cea9e7d7ff04799f6dc9ca6b289a66c747ac
Josephat-n <josphatnjoroge254@gmail.com>
2019-09-30 22:36:53 +0300

    add toggle effect for the description to the development and product management images

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,27 @@
 $(document).ready(function(){
-  $("img#design").click(function(){
+  $("img#design").click(function(){ //disp design content
     $(this).hide();
     $("p#des").css("display", "block");    
     });    
-  $("p#des").click(function(){
+  $("p#des").click(function(){ //hide design content
     $(this).hide();
     $("img#design").show();
   }); 
+  $("img#development").click(function(){ //disp development  content
+    $(this).hide();
+    $("p#dev").css("display", "block");    
+    });    
+  $("p#dev").click(function(){ //hide development content
+    $(this).hide();
+    $("img#development").show();
+  }); 
+  $("img#product").click(function(){ //disp product content
+    $(this).hide();
+    $("p#pdct").css("display", "block");    
+    });    
+  $("p#pdct").click(function(){ //hide product content
+    $(this).hide();
+    $("img#product").show();
+  }); 
+
 });
